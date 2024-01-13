233aaf2b40a68ee6ca6a29fca57c2967d2bb8c3b
Jay <jay@mobilelegalsolutions.com>
2024-01-12 19:13:24 -0800

    followed the course and built an app; went further and addedhardcoded contact sharing, going to make it serve from a db in the future

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,139 @@
 import 'package:flutter/material.dart';
+import 'package:share_plus/share_plus.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(BsCard());
+}
+
+class BsCard extends StatelessWidget {
+  const BsCard({super.key});
+
+  void shareContact() {
+    // Update this in the future to dynamically serve user data from a database.
+    Share.share(
+        'Contact Karl\nPhone: +1-234-567-8901\nEmail: abc@placeholder.com');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
+        appBar: AppBar(
+          backgroundColor: Colors.teal,
+        ),
+        backgroundColor: Colors.grey,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 55.0,
+                backgroundImage: AssetImage('images/dchad.png'),
+              ),
+              Text(
+                'Karl',
+                style: TextStyle(
+                    fontFamily: 'Lato',
+                    fontSize: 30.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
+              ),
+              Text(
+                'DRG Miner',
+                style: TextStyle(
+                    fontFamily: 'Source Sans Pro',
+                    fontSize: 20.0,
+                    letterSpacing: 2.5,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.white,
+                ),
+              ),
+              Card(
+                color: Colors.grey.shade700,
+                margin: EdgeInsets.symmetric(vertical: 5.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.white,
+                    size: 30.0,
+                  ),
+                  title: Text(
+                    '+1-234-567-8901',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                color: Colors.grey.shade700,
+                margin: EdgeInsets.symmetric(vertical: 5.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.white,
+                    size: 30.0,
+                  ),
+                  title: Text(
+                    'abc@placeholder.com',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+              Tooltip(
+                  message: 'Share your contact details',
+                  child: ElevatedButton(
+                    onPressed: shareContact,
+                    child: Text(
+                      'Share Contact',
+                      style: TextStyle(
+                        color: Colors.white, // White text color
+                      ),
+                    ),
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all<Color>(
+                          Colors.grey.shade700), // Grey background
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(18.0),
+                        ),
+                      ),
+                      padding: MaterialStateProperty.all<EdgeInsets>(
+                          EdgeInsets.all(15)),
+                    ),
+                  ))
+            ],
+          ),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.business),
+              label: 'Business',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.settings),
+              label: 'Settings',
+            ),
+          ],
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
